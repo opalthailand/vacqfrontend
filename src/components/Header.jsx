8156e677bd680545a00bc6f0e9c964e53bfb59e9
opalthailand <opalthailand@gmail.com>
2023-03-30 19:28:23 +0700

    Finish frontend Assignment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
-import {FaSignInAlt, FaSignOutAlt, FaUser} from 
+import {FaSignInAlt, FaSignOutAlt, FaUser} from
 'react-icons/fa'
-import {Link} from 'react-router-dom'
-
+import {Link,useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice'
 function Header(){
+    const navigate = useNavigate();
+    const dispatch = useDispatch()
+    const {user} = useSelector((state)=>state.auth)
+    const onLogout =()=>{ 
+        dispatch(logout()) 
+        dispatch(reset()) 
+        navigate('/')
+    }
 
      return (
         <header className='header'>
@@ -10,6 +19,12 @@ function Header(){
                 <Link to='/'>Support Desk</Link> 
             </div>
             <ul> 
+                {user?( <li>
+                    <button className='btn' onClick={onLogout}>
+                        <FaSignOutAlt />Logout
+                    </button>
+                </li>):(
+                <>
                 <li>
                     <Link to='/login'>
                         <FaSignInAlt />Login
@@ -20,6 +35,7 @@ function Header(){
                         <FaUser />Register
                     </Link>
                 </li>
+                </>)}
             </ul>
         </header>
     ) 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom' 
+import {Link} from 'react-router-dom'
 import {FaQuestionCircle, FaTicketAlt}from 'react-icons/fa'
 function Home(){
 return( 
